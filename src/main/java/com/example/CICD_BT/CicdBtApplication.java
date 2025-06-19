14edb0e718a8405ff9a63176f693a137f3a5f635
hoangmai29 <mnh290905@gmail.com>
2025-06-19 16:19:53 +0700

    Add files via upload

---
+++ src/main/java/com/example/CICD_BT/CicdBtApplication.java
@@ -0,0 +1,13 @@
+package com.example.CICD_BT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdBtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdBtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CICD_BT/CicdBtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CICD_BT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdBtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
